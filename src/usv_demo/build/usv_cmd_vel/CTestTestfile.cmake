7eb32196c8c9adb3a3feee7cd775600f962d594f
LuoLuoOnCall <905543470@qq.com>
2025-09-04 18:15:08 +0800

    Initial commit: Add vrx workspace

--- src/usv_demo/build/usv_cmd_vel/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wubin/vrx_ws/src/usv_demo/src/usv_cmd_vel
-# Build directory: /home/wubin/vrx_ws/src/usv_demo/build/usv_cmd_vel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/usv_demo/build/usv_pub_odom/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wubin/vrx_ws/src/usv_demo/src/usv_pub_odom
-# Build directory: /home/wubin/vrx_ws/src/usv_demo/build/usv_pub_odom
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
